f482e9e1ef49437181255323f9c9b036202b4f34
赵明宇 <498973030@qq.com>
2017-06-15 15:27:09 +0800

    Site updated: 2017-06-15 15:27:09

--- js/scroll.js
+++ js/scroll.js
@@ -4,7 +4,7 @@ window.onscroll = function() {
     var header = document.getElementsByClassName('site-header');
     if (document.body.scrollTop !== 0) {
         header[0].className = 'site-header site-header-with-shadow';
-        if (document.body.scrollTop < lastScrollTop) {
+        if (document.body.scrollTop > lastScrollTop) {
             header[0].className = 'site-header site-header-hidden';
         }
         lastScrollTop = document.body.scrollTop;
